cc2aa7194e77bb8beb8ba8831ca60d5b8734e333
stephaniecoleman <sproutwingsandfly@gmail.com>
2014-08-12 20:09:40 -0400

    Modify link_helper.

--- app/helpers/easymarklet/link_helper.rb
+++ app/helpers/easymarklet/link_helper.rb
@@ -1,7 +1,18 @@
 module Easymarklet::LinkHelper
-
-  def easymarklet_js(manifest = 'bookmarklet.js')
-    "javascript:(function(){var script=document.createElement('SCRIPT');script.src='#{root_url}assets/#{manifest}';document.body.appendChild(script);})()"
+	
+	def easymarklet_js(manifest = 'bookmarklet.js')
+  	if Rails.env == "production"
+	    "javascript:(function(){var script=document.createElement('SCRIPT');script.src='http://localhost:3000#{javascript_path(manifest)}';document.body.appendChild(script);})()"
+	 	else
+	    "javascript:(function(){var script=document.createElement('SCRIPT');script.src='http://localhost:3000/assets/bookmarklet.js';document.body.appendChild(script);})()"
+ 		end
   end
 
+
 end
+
+
+
+  # def easymarklet_js(manifest = 'bookmarklet.js')
+  #   "javascript:(function(){var script=document.createElement('SCRIPT');script.src='#{root_url}assets/#{manifest}';document.body.appendChild(script);})()"
+  # end
