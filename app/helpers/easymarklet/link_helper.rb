cb346c33e2f41eec353a30ba50ea07b8631392e3
stephaniecoleman <sproutwingsandfly@gmail.com>
2014-08-12 20:11:53 -0400

    Added commented out code from other versions.

--- app/helpers/easymarklet/link_helper.rb
+++ app/helpers/easymarklet/link_helper.rb
@@ -12,7 +12,20 @@ module Easymarklet::LinkHelper
 end
 
 
+	# !!!!!!!!!!! Original Gem Code !!!!!!!!!!
 
   # def easymarklet_js(manifest = 'bookmarklet.js')
   #   "javascript:(function(){var script=document.createElement('SCRIPT');script.src='#{root_url}assets/#{manifest}';document.body.appendChild(script);})()"
   # end
+
+
+
+  # !!!!!!!!!!! Steph and Blake Code !!!!!!!!!!
+
+	# def easymarklet_js(manifest = 'bookmarklet.js')
+	# 	if Rails.env == "production"
+	#     "javascript:(function(){var script=document.createElement('SCRIPT');script.src='#{root_url.gsub(":3000/", "")}#{javascript_path(manifest)}';document.body.appendChild(script);})()"
+	#  	else
+	#     "javascript:(function(){var script=document.createElement('SCRIPT');script.src='http://localhost:3000/assets/bookmarklet.js';document.body.appendChild(script);})()"
+	# 		end
+	# end
